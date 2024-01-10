7417dcb83a4bee7cd2c66f42134976c233a65ce3
rim109 <fla7722@naver.com>
2024-01-10 12:30:57 +0900

    feat: 회원가입 로직

--- src/main/java/com/example/springjwt/controller/MainController.java
+++ src/main/java/com/example/springjwt/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.springjwt.Controller;
+package com.example.springjwt.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
